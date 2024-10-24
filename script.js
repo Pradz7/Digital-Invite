899bbbceab090487ce8270fccec07f25abfb8e38
Faza Ar Rantisi <147687657+KanzeroG@users.noreply.github.com>
2024-10-24 18:00:32 +0700

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,97 @@
-(function() {
-    emailjs.init(gWQ_WhSNQYpsC_KBW); // Replace with your EmailJS user ID
+const containerPage = document.querySelector('.customForm');
+const form = document.querySelector('form');
+const userLastname = document.getElementById('lastname');
+const userFirstname = document.getElementById('firstname');
+const userEmail = document.getElementById('email');
+const userMessage = document.getElementById('message');
+const allFormField = document.querySelectorAll('.form-control');
 
-    document.getElementById('orderForm').addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
+// REGEX
+const nameRegex = /^([a-z|\s]{3,15})?([-]{0,1})?([a-z|\s]{3,15})$/;
+const emailRegex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
 
-        const templateParams = {
-            name: document.getElementById('InputName1').value,
-            phone: document.getElementById('InputPhoneNumber1').value,
-            email: document.getElementById('InputEmail').value,
-        };
+// INPUTS
+userLastname.addEventListener('input', () => checkInput(userLastname, nameRegex));
+userFirstname.addEventListener('input', () => checkInput(userFirstname, nameRegex));
+userEmail.addEventListener('input', () => checkInput(userEmail, emailRegex));
+userMessage.addEventListener('input', checkMessage);
 
-        emailjs.send(service_lpwjisp, template_ecoqixs, templateParams)
-            .then(function(response) {
-                console.log('SUCCESS!', response.status, response.text);
-                alert('Your order has been submitted successfully!');
-            }, function(error) {
-                console.log('FAILED...', error);
-                alert('Failed to send your order. Please try again later.');
-            });
-    });
-})();
+function checkInput(input, regex) {
+    const inputValue = input.value;
+    if (!regex.test(inputValue)) {
+        input.classList.add('is-invalid');
+        input.classList.remove('is-valid');
+    } else {
+        input.classList.add('is-valid');
+        input.classList.remove('is-invalid');
+    };
+    if (inputValue === '') input.classList.remove('is-invalid');
+};
+
+function checkMessage() {
+    const messageValue = userMessage.value;
+    if (messageValue.length < 10) {
+        userMessage.classList.add('is-invalid');
+        userMessage.classList.remove('is-valid');
+    } else {
+        userMessage.classList.add('is-valid');
+        userMessage.classList.remove('is-invalid');
+    };
+    if (messageValue === '') userMessage.classList.remove('is-invalid');
+};
+
+// ALERT
+function showAlert(type, message) {
+    const alert = document.createElement('div');
+    alert.classList.add('alert', type, 'fade', 'show', 'text-center');
+    alert.setAttribute('role', 'alert');
+    alert.innerHTML = message;
+    containerPage.insertBefore(alert, form);
+    setTimeout(() => {
+        document.querySelector('.alert').remove();
+    }, 1000);
+}
+
+// SUBMIT
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (userLastname.classList.contains('is-valid') && userFirstname.classList.contains('is-valid') && userEmail.classList.contains('is-valid') && userMessage.classList.contains('is-valid')) {
+        sendEmail();
+        showAlert('alert-success', 'Message sent !');
+        form.reset();
+        allFormField.forEach(input => input.classList.remove('is-valid'));
+    } else showAlert('alert-danger', 'Please fill in all the fields correctly !');
+});
+
+// Emailjs
+function sendEmail() {
+    let templateParams = {
+        lastname: userLastname.value,
+        firstname: userFirstname.value,
+        email: userEmail.value,
+        message: userMessage.value,
+    };
+
+    const serviceID = 'service_p3vhj5y';
+    const templateID = 'template_23w1kxp';
+
+    emailjs
+        .send(serviceID, templateID, templateParams)
+        .then(res => console.log('SUCCESS!', res.status, res.text))
+        .catch(err => console.log('FAILED...', err));
+};
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault(); 
+
+      // Get the target section
+      const targetSection = document.querySelector(this.getAttribute('href'));
+
+      window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth'
+      });
+  });
+});
